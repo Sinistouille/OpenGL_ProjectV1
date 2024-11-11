a5a0d4e03256f20bb3d82840545a9053b1179705
Maxence Lagourgue <maxence.lagourgue@free.fr>
2024-11-11 11:12:05 +0100

    Initial commit to the main branch

--- main.c
+++ main.c
@@ -4,25 +4,22 @@
 
 #include <GLAD/glad.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
-#include <vulkan/vulkan.hpp>
-#include "wtypes.h"
-#include <cstdlib>
+#include <vulkan/vulkan.h>
+#include <wtypes.h>
+#include <stdio.h>
 
-using namespace std;
-
-void programme();
+void programme(void);
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 int main() {
-    VkInstance* instance = nullptr;
+    VkInstance* instance = NULL;
     if (!glfwInit()) {
         return -1;
     }
     programme();
     glfwTerminate();
-    delete instance;
+    free(instance);
     return 0;
 }
 
@@ -33,10 +30,10 @@ void programme() {
 
     // Récupération du mode de l'écran
     const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-    cout << "Refresh rate : " << mode->refreshRate << endl;
-    cout << "Bits par pixel : " << mode->redBits << " " << mode->greenBits << " " << mode->blueBits << endl;
+    //cout << "Refresh rate : " << mode->refreshRate << endl;
+    //cout << "Bits par pixel : " << mode->redBits << " " << mode->greenBits << " " << mode->blueBits << endl;
     if(!mode) {
-        cout << "Erreur lors de la récupération des dimensions de l'écran" << endl;
+        printf("Erreur lors de la récupération des dimensions de l'écran");
         return;
     }
 
@@ -48,18 +45,18 @@ void programme() {
     glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
 
     //creating the window
-    GLFWwindow *window = glfwCreateWindow(10, 10, "My Title", nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(10, 10, "My Title", NULL, NULL);
     //making window fullscreen
     glfwSetWindowMonitor(window, glfwGetPrimaryMonitor(), 0, 0, mode->width, mode->height, 40);
     //checking if the window is created
     if (!window){
-        cout << "Erreur lors de la creation de la fenetre" << endl;
+        printf("Erreur lors de la creation de la fenetre");
         return;
     }
     //debug
-    cout << "RefreshRate : " << glfwGetWindowAttrib(window,GLFW_REFRESH_RATE) << endl;
-    cout << "RedBits : " << glfwGetWindowAttrib(window,GLFW_RED_BITS) << endl;
-    cout << "GreenBits : " << glfwGetWindowAttrib(window,GLFW_GREEN_BITS) << endl;
+    //cout << "RefreshRate : " << glfwGetWindowAttrib(window,GLFW_REFRESH_RATE) << endl;
+    //cout << "RedBits : " << glfwGetWindowAttrib(window,GLFW_RED_BITS) << endl;
+    //cout << "GreenBits : " << glfwGetWindowAttrib(window,GLFW_GREEN_BITS) << endl;
     //adding window to the thread
     glfwMakeContextCurrent(window);
     glfwSetWindowUserPointer(window, &user);
